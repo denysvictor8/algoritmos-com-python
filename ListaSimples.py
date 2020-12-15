4d0ee9da8547f1625e5440d851fc98f54f8251d1
Denys Silva <71570170+denysvictor8@users.noreply.github.com>
2020-12-15 11:21:48 -0300

    Update ListaSimples.py

--- ListaSimples.py
+++ ListaSimples.py
@@ -2,12 +2,11 @@
 # esse arquivo está apenas inserindo
 # ainda irei termina-lo, se quiser praticar faça vc mesmo a funcao remover(), fica como licao de casa :)
 
-
 class No:
     def __init__(self, valor):
         self.valor = valor
         self.prox = None
-        # um no possui um valor e aponta para o seu proximo 'companheiro'
+        # um no possui um valor e seu proximo 'companheiro'
 
     def getValor(self):
         return self.valor
@@ -62,22 +61,50 @@ class ListaSimples:
 
     def mostrar(self):
         aux = self.prim
-        print("------")
         while aux != None:
             print(aux.getValor())
             aux = aux.getProx()
         print("------")
 
-lista = ListaSimples()
-lista.inserir("Denys Silva",0)
-lista.inserir("Chris Headfield",1)
-lista.inserir("Leon S. Kennedy",2)
-lista.inserir("Ada Wong",3)
-lista.mostrar()
+    # todo metodo a ser feito
+    def remover(self, indice):
+        if self.getTam() == 0:
+            print("lista vazia, remocao impossivel")
+        else:
+            if indice == 0:
+                # remover no indice zero
+                self.prim = self.prim.getProx()
+                #novoNo.setProx(self.prim)
+                #self.prim = novoNo
+            elif indice >= self.getTam():
+                # inserir no final
+                self.ultimo.setProx(novoNo)
+                self.ultimo = novoNo
+            else:
+                #inserir no meio
+                noAnterior = self.prim
+                noAtual = self.prim.getProx()
+                indiceAtual = 1
+                # percorre ate achar o item a ser removido da lista
+                while indiceAtual != None:
+                    if indiceAtual == indice:
+                        novoNo.setProx(noAtual)
+                        noAnterior.setProx(novoNo)
+                        break
+                    noAnterior = noAtual
+                    noAtual = noAtual.getProx()
+                    indiceAtual += 1
+        self.tam += 1
 
 lista = ListaSimples()
 lista.inserir("Denys Silva",0)
 lista.inserir("Chris Headfield",1)
 lista.inserir("Leon S. Kennedy",2)
-lista.inserir("Ada Wong",0)
+lista.inserir("Veronica",1)
 lista.mostrar()
+
+lista.remover(0)
+lista.mostrar()
+
+
+
